bd1830eff4b0abf45d40108159e0b7efb6f475d2
Yonatan Greenfeld <yonathan@jit.io>
2022-08-22 10:15:43 +0300

    Rename package

--- setup.py
+++ setup.py
@@ -12,20 +12,20 @@ def find_stubs(package):
 
 
 setup(
-    name="aws-requests-auth-stubs",
+    name="aws_requests_auth-stubs",
     maintainer="Yonatan Greenfeld",
     maintainer_email="yonatangreenfeld97@gmail.com",
     description="PEP 561 type stubs for aws-requests-auth",
     url="https://github.com/YonatanGreenfeld/aws-requests-auth-stubs",
     license="MIT",
     version="0.4.3",
-    packages=["aws-requests-auth-stubs"],
+    packages=["aws_requests_auth-stubs"],
     # PEP 561 requires these
     install_requires=[
         "aws-requests-auth>=0.4.3",
         "botocore-stubs>=1.27.42",
         'typing_extensions>=3.7.4; python_version<"3.8"',
     ],
-    package_data=find_stubs("aws-requests-auth-stubs"),
+    package_data=find_stubs("aws_requests_auth-stubs"),
     zip_safe=False,
 )
