6a10c94a722223179c4e3927bc858dde0eff7bcf
Yonatan Greenfeld <yonathan@jit.io>
2022-08-22 09:08:49 +0300

    Replace setup.cfg with setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,31 @@
-from __future__ import annotations
 from setuptools import setup
+import os
 
-setup()
+
+def find_stubs(package):
+    stubs = []
+    for root, dirs, files in os.walk(package):
+        for file in files:
+            path = os.path.join(root, file).replace(package + os.sep, "", 1)
+            stubs.append(path)
+    return {package: stubs}
+
+
+setup(
+    name="aws-requests-auth-stubs",
+    maintainer="Yonatan Greenfeld",
+    maintainer_email="yonatangreenfeld97@gmail.com",
+    description="PEP 561 type stubs for aws-requests-auth",
+    url="https://github.com/YonatanGreenfeld/aws-requests-auth-stubs",
+    license="MIT",
+    version="0.4.3",
+    packages=["aws-requests-auth-stubs"],
+    # PEP 561 requires these
+    install_requires=[
+        "aws-requests-auth>=0.4.3",
+        "botocore-stubs>=1.27.42",
+        'typing_extensions>=3.7.4; python_version<"3.8"',
+    ],
+    package_data=find_stubs("aws-requests-auth-stubs"),
+    zip_safe=False,
+)
