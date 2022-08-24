138e9df7e3b681ff8c18b965b452b5c25a7afb40
Yonatan Greenfeld <yonathan@jit.io>
2022-08-24 08:32:49 +0300

    Fix flake8 stub issues and add overload

--- aws_requests_auth-stubs/aws_auth.pyi
+++ aws_requests_auth-stubs/aws_auth.pyi
@@ -1,10 +1,10 @@
 import sys
-from typing import Optional
+from typing import overload
 
-if sys.version_info <= (3, 8):
-    from typing import TypedDict
-else:
+if sys.version_info >= (3, 8):
     from typing_extensions import TypedDict
+else:
+    from typing import TypedDict
 
 from requests import PreparedRequest
 from requests.auth import AuthBase
@@ -15,9 +15,17 @@ AwsAuthHeader = TypedDict(
         "Authorization": str,
         "x-amz-date": str,
         "x-amz-content-sha256": bytes,
+    },
+)
+
+AwsAuthHeaderWithToken = TypedDict(
+    "AwsAuthHeaderWithToken",
+    {
+        "Authorization": str,
+        "x-amz-date": str,
+        "x-amz-content-sha256": bytes,
         "X-Amz-Security-Token": str,
     },
-    total=False,
 )
 
 def sign(key: bytes, message: str) -> bytes: ...
@@ -29,7 +37,7 @@ class AWSRequestsAuth(AuthBase):
     aws_host: str
     aws_region: str
     service: str
-    aws_token: Optional[str] = None
+    aws_token: str | None = ...
 
     def __init__(
         self,
@@ -38,12 +46,17 @@ class AWSRequestsAuth(AuthBase):
         aws_host: str,
         aws_region: str,
         aws_service: str,
-        aws_token: Optional[str] = None,
+        aws_token: str | None = ...,
     ) -> None: ...
     def get_aws_request_headers_handler(self, r: PreparedRequest): ...
+    @overload
+    def get_aws_request_headers(
+        self, r: PreparedRequest, aws_access_key: str, aws_secret_access_key: str, aws_token: None
+    ) -> AwsAuthHeader: ...
+    @overload
     def get_aws_request_headers(
         self, r: PreparedRequest, aws_access_key: str, aws_secret_access_key: str, aws_token: str
-    ) -> AwsAuthHeader: ...
+    ) -> AwsAuthHeaderWithToken: ...
     @classmethod
     def get_canonical_path(cls, r: PreparedRequest) -> str: ...
     @classmethod
--- aws_requests_auth-stubs/boto_utils.pyi
+++ aws_requests_auth-stubs/boto_utils.pyi
@@ -1,21 +1,20 @@
 import sys
-from typing import Optional
 
 from botocore.session import Session
 
 from .aws_auth import AWSRequestsAuth
 
-if sys.version_info <= (3, 8):
-    from typing import TypedDict
-else:
+if sys.version_info >= (3, 8):
     from typing_extensions import TypedDict
+else:
+    from typing import TypedDict
 
 class Credentials(TypedDict):
     aws_access_key: str
     aws_secret_access_key: str
     aws_token: str
 
-def get_credentials(credentials_obj: Optional[Session] = None): ...
+def get_credentials(credentials_obj: Session | None = ...): ...
 
 class BotoAWSRequestsAuth(AWSRequestsAuth):
     def __init__(self, aws_host: str, aws_region: str, aws_service: str): ...
